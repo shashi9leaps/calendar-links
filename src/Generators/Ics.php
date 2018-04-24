0816629465c93c6d1cdf2f392764788b101903cb
shashi9leaps <shashi@nineleaps.com>
2018-04-24 15:12:08 +0530

    Changed time format to 'Ymd\THis\Z'

--- src/Generators/Ics.php
+++ src/Generators/Ics.php
@@ -14,8 +14,8 @@ class Ics implements Generator
         $url .= 'BEGIN:VCALENDAR%0A';
         $url .= 'VERSION:2.0%0A';
         $url .= 'BEGIN:VEVENT%0A';
-        $url .= 'DTSTART:'.$link->from->format('Ymd\THis').'%0A';
-        $url .= 'DTEND:'.$link->to->format('Ymd\THis').'%0A';
+        $url .= 'DTSTART:'.$link->from->format('Ymd\THis\Z').'%0A';
+        $url .= 'DTEND:'.$link->to->format('Ymd\THis\Z').'%0A';
         $url .= "SUMMARY:{$link->title}%0A";
 
         if ($link->description) {
